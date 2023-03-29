fc286c1ca820f4d7a43394129f0d28d979a7095f
grindelf.perlomutrovij@gmail.com <grindelf.perlomutrovij@gmail.com>
2023-03-29 11:30:56 +0300

    add DecisionMaker

---
+++ src/main/kotlin/to/deepstorage/notebooksTask/DecisionMaker.kt
@@ -0,0 +1,25 @@
+package to.deepstorage.notebooksTask
+
+object DecisionMaker {
+
+    fun paretoProcessor(entries: List<Entry>): List<Entry> {
+        throw NotImplementedError();
+    }
+
+    fun lowestCriteria(entries: List<Entry>): List<Entry> {
+        throw NotImplementedError();
+    }
+
+    fun subOptimisationMethod(entries: List<Entry>): List<Entry> {
+        throw NotImplementedError();
+    }
+
+    fun lexicographicalMethod(entries: List<Entry>): List<Entry> {
+        throw NotImplementedError();
+    }
+
+    fun commonCriteriaProcessor(entries: List<Entry>): List<Entry> {
+        throw NotImplementedError();
+    }
+
+}
